2be4a2ec53f187286365c2bab68f5aa1bf2db628
opussf <github@zz9-za.com>
2020-06-02 14:26:53 -0700

    Works better
    Courage is fear holding on a minute longer.

--- src/UnusedGear.lua
+++ src/UnusedGear.lua
@@ -108,7 +108,8 @@ function UnusedGear.VARIABLES_LOADED()
 	UnusedGear.myIgnoreItems = UnusedGear_savedata[UnusedGear.realm][UnusedGear.name].ignoreItems
 end
 function UnusedGear.PLAYER_LEAVING_WORLD()
-	local lastMerchantShow = ( UnusedGear_savedata[UnusedGear.realm][UnusedGear.name].lastMerchantShow-1 or time() - 3600 )
+	local lastMerchantShow = ( UnusedGear_savedata[UnusedGear.realm][UnusedGear.name].lastMerchantShow and
+			UnusedGear_savedata[UnusedGear.realm][UnusedGear.name].lastMerchantShow-1 or time() - 3600 )
 	for link, item in pairs( UnusedGear.myItemLog ) do
 		if( ( item.lastSeen and item.lastSeen < lastMerchantShow ) or not item.lastSeen ) then
 			UnusedGear.myItemLog[link] = nil
